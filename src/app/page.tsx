4aa04b01c72aff9f87ea5a0fd9649e607ab1b3c9
Alymuhammad Bijani <a.bijani@wustl.edu>
2025-09-26 13:34:58 -0500

    analytics vercel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import { redirect } from "next/navigation";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   redirect("/signup");
+  return (
+    <>
+      <Analytics />
+    </>
+  );
 }
